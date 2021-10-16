86dac49d61405ca2a742b0c0bce0570c40e20f5c
Sam Kessler <42785937+sholom1@users.noreply.github.com>
2021-10-15 22:39:05 -0400

    Added super basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float moveForce;
+    private new Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(MoveDelta * moveForce, ForceMode2D.Impulse);
+    }
+    private Vector2 MoveDelta;
+    public void OnPlayerMove(CallbackContext ctx)
+    {
+        MoveDelta = ctx.ReadValue<Vector2>();
     }
 }
